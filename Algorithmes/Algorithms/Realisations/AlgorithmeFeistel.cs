cc43509567099688625047cca614fcc62ef478d4
Pires "Indorie" Lucas <94078903+IndorieSama@users.noreply.github.com>
2022-10-27 11:50:16 +0200

    Pbox caca

--- Algorithmes/Algorithms/Realisations/AlgorithmeFeistel.cs
+++ Algorithmes/Algorithms/Realisations/AlgorithmeFeistel.cs
@@ -9,7 +9,7 @@ namespace CryptoClient.Algorithmes.Algorithms.Realisations
     public class AlgorithmeFeistel
     {
         string[] sbox;
-
+        
         private string HexToBin32(string hex)
         {
             return "";
@@ -17,7 +17,31 @@ namespace CryptoClient.Algorithmes.Algorithms.Realisations
 
         public string PBox(string message)
         {
-            return "";
+            //fill the ints tab with int between 0 and 31
+            //each number is present only once
+
+
+            int index;
+            int[] intsRes = new int[32];
+            
+            int[] ints = new int[32] {1,4,6,18,28,12,24,3,17,21,8,5,2,7,26,9,31,20,25, 14, 19, 13, 3, 10, 15, 16, 27, 30, 29, 11, 22, 23 };
+            
+            
+            for (int i = 0; i < ints.Length; i++)
+            {
+                intsRes[i] = 0;
+            }
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                index = ints[i];
+
+                if (intsRes[index] != message[i])
+                {
+                    intsRes[index] = message[i];
+                }
+            }
+            return intsRes.ToString();
         }
 
         public string SBox(string message)
