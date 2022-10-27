8c5958c8063b876184346ea9938d1cfe81d7ded4
MarcGrandvincent <105056008+MarcGrandvincent@users.noreply.github.com>
2022-10-27 10:51:17 +0200

    Squelette AlgoFeistel et ntm lucas

---
+++ Algorithmes/Algorithms/Realisations/Transposition/AlgorithmeFeistel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CryptoClient.Algorithmes.Algorithms.Realisations.Transposition
+{
+    public class AlgorithmeFeistel
+    {
+        string[] sbox;
+
+        private string HexToBin32(string hex)
+        {
+            return "";
+        }
+
+        public string PBox(string message)
+        {
+            return "";
+        }
+
+        public string SBox(string message)
+        {
+            return "";
+        }
+
+        public string EBox(string message)
+        {
+            return "";
+        }
+
+        private string Add32(string nb1, string nb2)
+        {
+            return "";
+        }
+
+        public string F(string message, string cle)
+        {
+            return "";
+        }
+
+        public string ClePartielle(string cle, int numTour)
+        {
+            return "";
+        }
+
+        private string TourDechiffrement(string message, string cle, int numTour)
+        {
+            return "";
+        }
+
+    }
+}
